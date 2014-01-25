9c2b8ace084649280d6293480ae0c7cca0de0e6e
Runar Flåten <runarflaaten@gmail.com>
2014-01-25 18:00:45 +0100

    added all changes

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,34 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category, Page
+
+def category (request, category_name_url):
+    context = RequestContext (request)
+
+    category_name = category_name_url.replace('_', ' ')
+
+    context_dict = {'category_name': category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+
+        pages = Page.objects.filter (category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
+    
 
 def index(request):
     context = RequestContext (request)
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by ('-likes')[:5]
+    context_dict = {'categories': category_list}
     
     return render_to_response ('rango/index.html', context_dict, context)
 
