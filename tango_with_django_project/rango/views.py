5d66f786df9739628422aa14d027e7266e142061
Runar Flåten <runarflaaten@gmail.com>
2014-01-28 21:28:22 +0100

    Finished chapter 14

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,32 +1,45 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 from rango.forms import CategoryForm, PageForm
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from datetime import datetime
+from rango.bing_search import run_query
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
-@login_required
-def restricted (request):
-    context = RequestContext (request)
-    context_dict = {}
+def get_category_list ():
+    cat_list = Category.objects.all ()
 
-    return render_to_response ('rango/restricted.html',
-                               context_dict,
-                               context)
-    
+    for cat in cat_list:
+        cat.url = encode_url (cat.name)
+
+    return cat_list
+
 def encode_url (category_name_url):
     return category_name_url.replace (' ', '_')
 
 def decode_url (category_name_url):
     return category_name_url.replace('_', ' ')
 
+@login_required
+def restricted (request):
+    context = RequestContext (request)
+    context_dict = {}
+    context_dict['cat_list'] = get_category_list ()
+
+    return render_to_response ('rango/restricted.html',
+                               context_dict,
+                               context)
+
 def about (request):
     context = RequestContext (request)
     context_dict = {}
+    context_dict['cat_list'] = get_category_list ()
 
     if request.session.get ('visits'):
         count = request.session.get ('visits')
@@ -40,14 +53,19 @@ def about (request):
 def category (request, category_name_url):
     context = RequestContext (request)
 
-    category_name = category_name_url.replace('_', ' ')
+    category_name = decode_url (category_name_url)
+    cat_list = get_category_list ()
 
-    context_dict = {'category_name': category_name, 'category_name_url': category_name_url}
+    context_dict = {'category_name': category_name,
+                    'category_name_url': category_name_url,
+                    'cat_list': cat_list}
+    
 
+    
     try:
         category = Category.objects.get(name=category_name)
 
-        pages = Page.objects.filter (category=category)
+        pages = Page.objects.filter (category=category).order_by ('-views')
 
         context_dict['pages'] = pages
 
@@ -56,14 +74,25 @@ def category (request, category_name_url):
     except Category.DoesNotExist:
         pass
 
+    if request.method == 'POST':
+        query = request.POST ['query'].strip()
+
+        if query:
+            result_list = run_query (query)
+            context_dict['result_list'] = result_list
+        
     return render_to_response('rango/category.html', context_dict, context)
-    
+
+
 
 def index(request):
     context = RequestContext (request)
     category_list = Category.objects.order_by ('-likes')[:5]
     page_list = Page.objects.order_by ('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': page_list}
+    cat_list = get_category_list ()
+    context_dict = {'categories': category_list,
+                    'pages': page_list,
+                    'cat_list': cat_list}
 
     for category in category_list:
         category.url = encode_url (category.name)
@@ -85,24 +114,15 @@ def index(request):
         request.session['last_visit'] = str(datetime.now ())
         request.session['visits'] = 1
 
-    # Client side cookie
-    # visits = int (request.COOKIES.get ('visits', '0'))
-
-    # if request.COOKIES.has_key ('last_visit'):
-    #     last_visit = request.COOKIES['last_visit']
-    #     last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-
-    #     if (datetime.now () - last_visit_time).seconds > 5:
-    #         response.set_cookie ('visits', visits+1)
-    #         response.set_cookie ('last_visit', datetime.now ())
-    # else:
-    #     response.set_cookie ('last_visit', datetime.now ())
-        
     return response
 
 @login_required
 def add_category (request):
     context = RequestContext (request)
+    context_dict = {}
+    cat_list = get_category_list ()
+    context_dict['cat_list'] = cat_list
+    
 
     if request.method == 'POST':
         form = CategoryForm (request.POST)
@@ -117,13 +137,15 @@ def add_category (request):
     else:
         form = CategoryForm ()
 
-    return render_to_response ('rango/add_category.html', {'form': form}, context)
+    context_dict['form'] = form
+    return render_to_response ('rango/add_category.html', context_dict, context)
 
 
 @login_required
 def add_page (request, category_name_url):
     context = RequestContext (request)
     context_dict = {}
+    context_dict['cat_list'] = get_category_list ()
     
     category_name = decode_url (category_name_url)
     if request.method == 'POST':
@@ -166,6 +188,7 @@ def register (request):
     context = RequestContext (request)
 
     context_dict = {}
+    context_dict['cat_list'] = get_category_list ()
     registered = False
 
     if request.method == 'POST':
@@ -208,6 +231,7 @@ def register (request):
 def user_login (request):
     context = RequestContext (request)
     context_dict = {}
+    context_dict['cat_list'] = get_category_list ()
 
     if request.method == 'POST':
         username = request.POST['username']
@@ -241,3 +265,61 @@ def user_logout (request):
     logout (request)
 
     return HttpResponseRedirect('/rango/')
+
+def search (request):
+    context = RequestContext (request)
+    context_dict = {}
+    context_dict['cat_list'] = get_category_list ()
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST ['query'].strip()
+
+        if query:
+            result_list = run_query (query)
+
+    context_dict['result_list'] = result_list
+    return render_to_response ('rango/search.html',
+                               context_dict,
+                               context)
+
+def profile (request):
+    context = RequestContext (request)
+    context_dict = {}
+    context_dict['cat_list'] = get_category_list ()
+    u = User.objects.get (username=request.user)
+
+    try:
+        up = UserProfile.objects.get (user=u)
+    except:
+        up = None
+
+    context_dict ['user'] = u
+    context_dict ['userprofile'] = up
+
+    return render_to_response ('rango/profile.html',
+                               context_dict,
+                               context)
+    
+
+def track_url (request):
+    context = RequestContext (request)
+    page_id = None
+    url = '/rango/'
+
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+            print page_id
+            try:
+                page = Page.objects.get(id=page_id)
+                page.views = page.views + 1
+                page.save ()
+                url = page.url
+
+            except:
+                pass
+
+            
+    
+    return redirect (url)
